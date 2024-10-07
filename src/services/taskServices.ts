eb4f68e3556c8a438ba98b16877ab0f90cfb6ae2
faculdademaely <unifanengsoftware@gmail.com>
2024-10-07 20:01:13 -0300

    07/10/2024

---
+++ src/services/taskServices.ts
@@ -0,0 +1,69 @@
+import { describe } from "node:test";
+import {DevDataSource} from "../connections/db_Dev";
+import {Task} from "../models/task";
+
+const cursor = DevDataSource.getRepository(Task)
+
+type newTaskResquest = {
+    description: string,
+    date_task: Date
+}
+
+type findTaskRequest = {
+    id: string
+}
+
+type updateTaskRequest ={
+    id: string
+    description: string,
+    date_task: Date
+}
+
+export class TaskService{
+    async creatTask({
+        description, date_task
+    }: newTaskResquest):Promise<Task | Error> {
+        const task = cursor.create({
+            description, date_task
+        })
+        await cursor.save(task)
+        return task
+    }
+    
+    async readOneTask({id}: findTaskRequest) :
+    Promise <Task | Error> {
+        const task = await cursor.findOne({where: {id}})
+        if(!task){
+            return new Error("Task not found!")
+        }
+        return task 
+    }
+    
+    async readAllTask(){
+        const tasks = await cursor.find()
+        return tasks 
+    }
+    
+    async updateTask( { id, description, date_task } : updateTaskRequest): Promise<Task | Error>{
+        const task = await cursor.findOne({where: {id}})
+        if(!task){
+            return new Error("Task not found!")
+        }
+
+        task.description = description ? description : task.description
+        task.date_task = date_task ? date_task : task.date_task
+
+        await cursor.save(task)
+        return task 
+    }
+    
+    async deleteTask( {id}:findTaskRequest): Promise<String | Error> {
+        const task = await cursor.findOne({where: {id}})
+        if(!task){
+            return new Error("Task not found!")
+        }
+        await cursor.delete(task.id)
+        return "Task removed successfully"
+        
+    }
+}
